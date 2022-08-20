911137b3eabfcfc89103cd13c778ff41f17c3c51
Edwin Carl Flores <edwincarlflores@gmail.com>
2022-08-20 16:10:04 +0800

    Added Vote db model and partial options property

--- src/pages/question/[id].tsx
+++ src/pages/question/[id].tsx
@@ -11,7 +11,16 @@ const QuestionPageContent: React.FC<{ id: string }> = ({ id }) => {
     return <div>Question not found</div>;
   }
 
-  return <div>{data?.question}</div>;
+  return (
+    <div className="flex flex-col p-8">
+      <div className="text-2xl font-bold">{data?.question}</div>
+      <div>
+        {(data?.options as string[])?.map((option, index) => (
+          <div key={`option-${index}`}>{option}</div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 const QuestionPage = () => {
