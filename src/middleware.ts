3a35d8519aab5346ea567d125f64ee24e4e8881e
Edwin Carl Flores <edwincarlflores@gmail.com>
2022-08-21 19:29:47 +0800

    Improved poll-token value by using nanoid

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,17 @@
 import { NextResponse, type NextRequest } from "next/server";
+import { nanoid } from "nanoid";
 
 export function middleware(req: NextRequest) {
-  const userCookie = req.cookies.get("userCookie");
+  const pollToken = req.cookies.get("poll-token");
 
-  if (userCookie) {
+  if (pollToken) {
     return;
   }
 
-  const random = Math.random().toString();
+  const token = nanoid();
 
   const res = NextResponse.next();
-  res.cookies.set("userCookie", random, { sameSite: "strict" });
+  res.cookies.set("poll-token", token, { sameSite: "strict" });
 
   return res;
 }
