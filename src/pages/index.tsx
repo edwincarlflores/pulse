57513ad13bdba31fad0ce5c9aed4409285cd1bd7
Edwin Carl Flores <edwincarlflores@gmail.com>
2022-08-26 02:20:44 +0800

    Added route for creating questions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,31 +3,6 @@ import type { NextPage } from "next";
 import { trpc } from "../utils/trpc";
 import Link from "next/link";
 
-const QuestionCreator: React.FC = () => {
-  const inputRef = React.useRef<HTMLInputElement>(null);
-  const client = trpc.useContext();
-  const { mutate, isLoading } = trpc.useMutation("questions.create", {
-    onSuccess: () => {
-      client.invalidateQueries(["questions.get-all-my-questions"]);
-      if (!inputRef.current) return;
-      inputRef.current.value = "";
-    },
-  });
-
-  return (
-    <input
-      ref={inputRef}
-      disabled={isLoading}
-      onKeyDown={(event) => {
-        if (event.key === "Enter") {
-          mutate({ question: event.currentTarget.value });
-          event.currentTarget.value = "";
-        }
-      }}
-    />
-  );
-};
-
 const Home: NextPage = () => {
   const { data, isLoading } = trpc.useQuery(["questions.get-all-my-questions"]);
 
@@ -38,18 +13,23 @@ const Home: NextPage = () => {
   return (
     <div className="flex flex-col p-6">
       <div className="flex flex-col">
-        <div className="text-2xl font-bold">Questions</div>
+        <div className="text-2xl font-bold">Your Questions</div>
         {data.map((question) => (
-          <Link key={question.id} href={`/question/${question.id}`}>
-            <a>
-              <div key={question.id} className="my-2">
-                {question.question}
-              </div>
-            </a>
-          </Link>
+          <div key={question.id} className="my-2 flex flex-col">
+            <Link href={`/question/${question.id}`}>
+              <a>
+                <div key={question.id} className="">
+                  {question.question}
+                </div>
+              </a>
+            </Link>
+            <span>Created on {question.createdAt.toDateString()}</span>
+          </div>
         ))}
       </div>
-      <QuestionCreator />
+      <Link href="/create">
+        <a>Create New Question</a>
+      </Link>
     </div>
   );
 };
