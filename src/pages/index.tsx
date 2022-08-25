f04b73a9525c6f5b6fac3e2ef2f5e81b2440cee4
Edwin Carl Flores <edwincarlflores@gmail.com>
2022-08-26 02:41:49 +0800

    Updated UI styling. Move create button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import type { NextPage } from "next";
 import { trpc } from "../utils/trpc";
 import Link from "next/link";
@@ -11,25 +10,29 @@ const Home: NextPage = () => {
   }
 
   return (
-    <div className="flex flex-col p-6">
-      <div className="flex flex-col">
+    <div className="flex w-screen flex-col items-stretch p-6">
+      <div className="header flex w-full justify-between">
         <div className="text-2xl font-bold">Your Questions</div>
+        <Link href="/create">
+          <a className="rounded bg-gray-300 p-4 text-gray-800">
+            Create New Question
+          </a>
+        </Link>
+      </div>
+      <div className="flex flex-col">
         {data.map((question) => (
-          <div key={question.id} className="my-2 flex flex-col">
-            <Link href={`/question/${question.id}`}>
-              <a>
+          <Link key={question.id} href={`/question/${question.id}`}>
+            <a>
+              <div className="my-2 flex flex-col">
                 <div key={question.id} className="">
                   {question.question}
                 </div>
-              </a>
-            </Link>
-            <span>Created on {question.createdAt.toDateString()}</span>
-          </div>
+                <span>Created on {question.createdAt.toDateString()}</span>
+              </div>
+            </a>
+          </Link>
         ))}
       </div>
-      <Link href="/create">
-        <a>Create New Question</a>
-      </Link>
     </div>
   );
 };
